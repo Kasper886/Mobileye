0084930981f58ce0c81d621f4e3fc568e39d1c77
Kasper886 <aleksbulgakov@gmail.com>
2022-06-19 18:42:51 +0300

    fixed

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-server" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = data.terraform_remote_state.mobileye-virginia.outputs.id
+  key_name               = data.terraform_remote_state.ssh_key.outputs.id
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web-servers.id]
   user_data              = <<EOF
