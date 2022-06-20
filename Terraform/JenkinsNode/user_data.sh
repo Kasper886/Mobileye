e6d49db10aaee50ca8c9e6cd6a18e35212b7579d
Kasper886 <aleksbulgakov@gmail.com>
2022-06-20 13:59:12 +0300

    last

--- Terraform/JenkinsNode/user_data.sh
+++ Terraform/JenkinsNode/user_data.sh
@@ -6,7 +6,23 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt install -y docker-ce
 sudo mkdir ~/Jenkins
-sudo cp Dockerfile ~/Jenkins
+sudo cd ~/Jenkins
+
+sudo cat << 'EOF' >> Dockerfile
+FROM jenkins/jenkins:2.332.3-jdk11
+USER root
+RUN apt-get update && apt-get install -y lsb-release
+RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \
+  https://download.docker.com/linux/debian/gpg
+RUN echo "deb [arch=$(dpkg --print-architecture) \
+  signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \
+  https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+RUN apt-get update && apt-get install -y docker-ce-cli
+USER jenkins
+RUN jenkins-plugin-cli --plugins "blueocean:1.25.5 docker-workflow:1.28"
+EOF
+
 sudo docker build -t myjenkins-blueocean:2.332.3-1 .
 sudo docker run --name jenkins-blueocean --restart=on-failure --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
