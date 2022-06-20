146cc655d3054f2b4f62ddf424534474dd532500
Kasper886 <aleksbulgakov@gmail.com>
2022-06-20 15:21:07 +0300

    last

--- Terraform/JenkinsNode/user_data.sh
+++ Terraform/JenkinsNode/user_data.sh
@@ -5,7 +5,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 sudo apt install -y docker-ce
-sudo su
 sudo mkdir ~/Jenkins
 sudo cd ~/Jenkins
 sudo touch ~/Jenkins/Dockerfile
@@ -34,7 +33,7 @@ RUN jenkins-plugin-cli --plugins "blueocean:1.25.5 docker-workflow:1.28"
 EOF
 
 sudo docker build -t myjenkins-blueocean:2.332.3-1 .
-
+sudo docker network create jenkins
 sudo docker run --name jenkins-blueocean --restart=on-failure --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
