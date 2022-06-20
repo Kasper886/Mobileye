eea16dc16cb3f61f42f1d21cc038caed2ab52bbd
Kasper886 <aleksbulgakov@gmail.com>
2022-06-20 14:07:58 +0300

    last

--- Terraform/JenkinsNode/user_data.sh
+++ Terraform/JenkinsNode/user_data.sh
@@ -5,6 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 sudo apt install -y docker-ce
+sudo su
 sudo mkdir ~/Jenkins
 sudo cd ~/Jenkins
 
