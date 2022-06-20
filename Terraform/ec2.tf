f15c945b249d5fc09cfd27073bbbc88596276a38
Kasper886 <aleksbulgakov@gmail.com>
2022-06-20 10:47:32 +0300

    fixed

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-server" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = data.aws_key_pair.virginia.key_name
+  key_name               = virginia
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web-servers.id]
   user_data              = file("user_data.sh")
